e4f1f009eb14f461cd74060b5b3f1559e17084bb
iuwizeyimana <iuwizeyimana@hawk.iit.edu>
2021-04-14 00:29:12 -0400

    changed max comp parameter

--- rose_conv.C
+++ rose_conv.C
@@ -13,24 +13,24 @@ void conv(hls::stream<int>& out, hls::stream<int>& w, hls::stream<int>& in)
   int i;
   int j;
   int _lt_var_ti;
-  for (_lt_var_ti = 0; _lt_var_ti <= 255; _lt_var_ti += 33) {
+  for (_lt_var_ti = 0; _lt_var_ti <= 255; _lt_var_ti += 14) {
     int _lt_var_row;
     for (_lt_var_row = 0; _lt_var_row <= 12; _lt_var_row += 7) {
       int _lt_var_col;
       for (_lt_var_col = 0; _lt_var_col <= 12; _lt_var_col += 7) {
         int _lt_var_to;
-        int out_buff[3][7][7];
-        for (_lt_var_to = 0; _lt_var_to <= 191; _lt_var_to += 3) {
+        int out_buff[17][7][7];
+        for (_lt_var_to = 0; _lt_var_to <= 191; _lt_var_to += 17) {
 #pragma HLS DATAFLOW
           load_w:
-          int w_buff[3][33][3][3];
+          int w_buff[17][14][3][3];
           int hold_w;
           int d;
           int c;
           int b;
           int a;
-          for (a = 0; a < 3; a++) {
-            for (b = 0; b < 33; b++) {
+          for (a = 0; a < 17; a++) {
+            for (b = 0; b < 14; b++) {
               for (c = 0; c < 3; c++) {
                 for (d = 0; d < 3; d++) {
 if(!w.empty()){w>> hold_w; w_buff[a][b][c][d] = hold_w;}
@@ -39,12 +39,12 @@ if(!w.empty()){w>> hold_w; w_buff[a][b][c][d] = hold_w;}
             }
           }
           load_in:
-          int in_buff[33][7][7];
+          int in_buff[14][7][7];
           int hold_in;
           int c1;
           int b1;
           int a1;
-          for (a1 = 0; a1 < 33; a1++) {
+          for (a1 = 0; a1 < 14; a1++) {
             for (b1 = 0; b1 < 7; b1++) {
               for (c1 = 0; c1 < 7; c1++) {
 if(!in.empty()){in>> hold_in; in_buff[a1][b1][c1] = hold_in;}
@@ -56,11 +56,11 @@ if(!in.empty()){in>> hold_in; in_buff[a1][b1][c1] = hold_in;}
             for (col = _lt_var_col; col <= (((12 < (_lt_var_col + 7 - 1))?12 : (_lt_var_col + 7 - 1))); col += 1) {
               for (i = 0; i < 3; i++) {
                 for (j = 0; j < 3; j++) {
-                  for (ti = _lt_var_ti; ti <= (((255 < (_lt_var_ti + 33 - 1))?255 : (_lt_var_ti + 33 - 1))); ti += 1) {
-#pragma HLS UNROLL factor = 33
-                    for (to = _lt_var_to; to <= (((191 < (_lt_var_to + 3 - 1))?191 : (_lt_var_to + 3 - 1))); to += 1) {
+                  for (ti = _lt_var_ti; ti <= (((255 < (_lt_var_ti + 14 - 1))?255 : (_lt_var_ti + 14 - 1))); ti += 1) {
+#pragma HLS UNROLL factor = 14
+                    for (to = _lt_var_to; to <= (((191 < (_lt_var_to + 17 - 1))?191 : (_lt_var_to + 17 - 1))); to += 1) {
                       
-#pragma HLS UNROLL factor = 3
+#pragma HLS UNROLL factor = 17
 /* 
 out[to][row][col] += (w[to][ti][i][j] * in[ti][row + i][col + j]);
 */ out_buff[to][row][col] += w_buff[to][ti][i][j] * in_buff[ti][row+i][col+j];
@@ -75,7 +75,7 @@ out[to][row][col] += (w[to][ti][i][j] * in[ti][row + i][col + j]);
         int c2;
         int b2;
         int a2;
-        for (a2 = 0; a2 < 3; a2++) {
+        for (a2 = 0; a2 < 17; a2++) {
           for (b2 = 0; b2 < 7; b2++) {
             for (c2 = 0; c2 < 7; c2++) {
 out<< out_buff[a2][b2][c2];
